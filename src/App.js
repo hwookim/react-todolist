0378e50227c040ff70dc3d72da0ed248b3bb1544
hwookim <gusdn9754@gmail.com>
2020-11-21 17:50:08 +0900

    chore: todolist 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="todoapp">
+        <div>
+          <h1>TODOS</h1>
+          <input id="new-todo-title" className="new-todo" placeholder="할일을 추가해주세요" autoFocus />
+        </div>
+        <div className="main">
+          <input className="toggle-all" type="checkbox" />
+            <ul id="todo-list" className="todo-list" />
+        </div>
+        <div className="count-container">
+          <span className="todo-count">총 <strong>0</strong> 개</span>
+          <ul className="filters">
+            <li>
+              <a className="filter all selected" href="#/">전체보기</a>
+            </li>
+            <li>
+              <a className="filter active" href="#/active">해야할 일</a>
+            </li>
+            <li>
+              <a className="filter completed" href="#/completed">완료한 일</a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("render TODOS", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/TODOS/i);
   expect(linkElement).toBeInTheDocument();
 });
