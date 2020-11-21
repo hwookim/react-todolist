2c1d388d1030588bfd6f42fbe8b0434471381bc4
hwookim <gusdn9754@gmail.com>
2020-11-21 18:35:04 +0900

    chore: prettier 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  endOfLine: "lf",
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "always",
+  printWidth: 80,
+};
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -6,23 +6,36 @@ function App() {
       <section className="todoapp">
         <div>
           <h1>TODOS</h1>
-          <input id="new-todo-title" className="new-todo" placeholder="할일을 추가해주세요" autoFocus />
+          <input
+            id="new-todo-title"
+            className="new-todo"
+            placeholder="할일을 추가해주세요"
+            autoFocus
+          />
         </div>
         <div className="main">
           <input className="toggle-all" type="checkbox" />
-            <ul id="todo-list" className="todo-list" />
+          <ul id="todo-list" className="todo-list" />
         </div>
         <div className="count-container">
-          <span className="todo-count">총 <strong>0</strong> 개</span>
+          <span className="todo-count">
+            총 <strong>0</strong> 개
+          </span>
           <ul className="filters">
             <li>
-              <a className="filter all selected" href="#/">전체보기</a>
+              <a className="filter all selected" href="#/">
+                전체보기
+              </a>
             </li>
             <li>
-              <a className="filter active" href="#/active">해야할 일</a>
+              <a className="filter active" href="#/active">
+                해야할 일
+              </a>
             </li>
             <li>
-              <a className="filter completed" href="#/completed">완료한 일</a>
+              <a className="filter completed" href="#/completed">
+                완료한 일
+              </a>
             </li>
           </ul>
         </div>
