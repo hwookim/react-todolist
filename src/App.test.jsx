fe4e0b9aa8fa20c4b07acac7140016c0f557d4ef
hwookim <gusdn9754@gmail.com>
2020-11-22 19:06:46 +0900

    refactor: 테스트, 프로덕션 폴더 분리

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("render TODOS", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/TODOS/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/test/App.test.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import App from "../main/App";
+
+describe("App", () => {
+  it("render TODOS", () => {
+    const { container } = render(<App />);
+
+    expect(container).toHaveTextContent("TODOS");
+  });
+});
