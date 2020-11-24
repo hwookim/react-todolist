9c7407edb874f9e464655645b5db214ff2d289e7
hwookim <gusdn9754@gmail.com>
2020-11-25 02:04:18 +0900

    refactor: Input placeholder 검증 방식 변경
     - screen을 경유한 검증 -> 랜더한 객체의 getByPlaceholderText를 직접 이용

--- src/test/component/TodoInput.test.jsx
+++ src/test/component/TodoInput.test.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import TodoInput from "../../main/component/TodoInput";
 
 describe("TodoInput", () => {
   it("render Input placeholder", () => {
-    render(<TodoInput />);
+    const { getByPlaceholderText } = render(<TodoInput />);
 
-    screen.getByPlaceholderText("할일을 추가해주세요");
+    getByPlaceholderText("할일을 추가해주세요");
   });
 });
