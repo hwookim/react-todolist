13963c885ce8c1e8278117a01ff44b73c83da401
hwookim <gusdn9754@gmail.com>
2020-12-08 00:26:40 +0900

    refactor: Describe-Context-It 패턴에 맞게 테스트 수정

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import context from "jest-plugin-context";
 
 import App from "../main/App";
 
 describe("App", () => {
-  it("render new task", () => {
-    const { container } = render(<App />);
-    const $input = container.querySelector(".new-todo");
-    const $list = container.querySelector(".todo-list");
+  const { container } = render(<App />);
+  const $input = container.querySelector(".new-todo");
+  const $list = container.querySelector(".todo-list");
 
+  context("add new task", () => {
     fireEvent.change($input, {
       target: { value: "Let's TDD" },
     });
@@ -19,6 +20,8 @@ describe("App", () => {
       charCode: 13,
     });
 
-    expect($list).toHaveTextContent("Let's TDD");
+    it("render new task", () => {
+      expect($list).toHaveTextContent("Let's TDD");
+    });
   });
 });
--- src/test/component/TodoInput.test.jsx
+++ src/test/component/TodoInput.test.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import context from "jest-plugin-context";
 
 import TodoInput from "../../main/component/TodoInput";
 
 describe("TodoInput", () => {
-  it("keypress enter to input, run method onInsert and clear value", () => {
-    const onInsert = jest.fn();
-    const { container } = render(<TodoInput onInsert={onInsert} />);
-    const $input = container.querySelector(".new-todo");
+  const onInsert = jest.fn();
+  const { container } = render(<TodoInput onInsert={onInsert} />);
+  const $input = container.querySelector(".new-todo");
 
+  context("with value", () => {
     fireEvent.change($input, {
       target: { value: "Let's TDD" },
     });
 
-    fireEvent.keyPress($input, {
-      key: "Enter",
-      code: 13,
-      charCode: 13,
-    });
+    it("keypress Enter run method onInsert by value and clear value", () => {
+      fireEvent.keyPress($input, {
+        key: "Enter",
+        code: 13,
+        charCode: 13,
+      });
 
-    expect(onInsert).toBeCalledWith("Let's TDD");
-    expect($input).toHaveValue("");
+      expect(onInsert).toBeCalledWith("Let's TDD");
+      expect($input).toHaveValue("");
+    });
   });
 });
