bfbb77fe104c85afc1fc599cfce34e341a9a8d50
hwookim <gusdn9754@gmail.com>
2020-12-09 22:00:54 +0900

    refactor: 테스트 구조 변경 및 완료 취소 테스트 작성

--- src/test/component/TodoItem.test.jsx
+++ src/test/component/TodoItem.test.jsx
@@ -5,23 +5,52 @@ import context from "jest-plugin-context";
 import TodoItem from "../../main/component/TodoItem";
 
 describe("TodoItem", () => {
-  const task = {
-    id: 1,
-    content: "Todo!",
-  };
-  const { container } = render(<TodoItem task={task} />);
+  function renderTodoItem(task) {
+    return render(<TodoItem task={task} />);
+  }
 
-  it("render task content", () => {
-    expect(container).toHaveTextContent(task.content);
+  context("with not completed task", () => {
+    const task = {
+      id: 1,
+      content: "Todo!",
+    };
+
+    it("render task content", () => {
+      const { container } = renderTodoItem(task);
+      expect(container).toHaveTextContent(task.content);
+    });
   });
 
   context("click toggle btn", () => {
+    const task = {
+      id: 1,
+      content: "Todo!",
+    };
+
+    const { container } = renderTodoItem(task);
     const $li = container.querySelector("li");
     const $btn = container.querySelector(".toggle");
     fireEvent.click($btn);
 
     it("add class", () => {
       expect($li).toHaveClass("completed");
+    });
+  });
+
+  context("click toggle btn twice", () => {
+    const task = {
+      id: 1,
+      content: "Todo!",
+    };
+
+    const { container } = renderTodoItem(task);
+    const $li = container.querySelector("li");
+    const $btn = container.querySelector(".toggle");
+    fireEvent.click($btn);
+    fireEvent.click($btn);
+
+    it("remove class", () => {
+      expect($li).not.toHaveClass("completed");
     });
   });
 });
