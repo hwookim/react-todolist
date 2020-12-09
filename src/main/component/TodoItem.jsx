daabd03a884427a8fa845be777cc8bdae6b852d2
hwookim <gusdn9754@gmail.com>
2020-12-09 21:16:30 +0900

    feat: 완료하기 기능 추가

--- src/main/component/TodoItem.jsx
+++ src/main/component/TodoItem.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoItem({ task }) {
+  const [completed, setCompleted] = useState(false);
+
+  const handleToggleTodo = () => {
+    setCompleted(!completed);
+  };
+
+  const getStatus = () => {
+    return completed ? "completed" : "";
+  };
+
   return (
-    <li>
+    <li className={getStatus()}>
       <div className="view">
-        <input className="toggle" type="checkbox" />
+        <input className="toggle" type="checkbox" onClick={handleToggleTodo} />
         <label className="label">{task.content}</label>
         <button className="destroy" />
       </div>
--- src/test/component/TodoItem.test.jsx
+++ src/test/component/TodoItem.test.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import context from "jest-plugin-context";
 
 import TodoItem from "../../main/component/TodoItem";
 
 describe("TodoItem", () => {
-  context("with task", () => {
-    const task = {
-      id: 1,
-      content: "Todo!",
-    };
+  const task = {
+    id: 1,
+    content: "Todo!",
+  };
+  const { container } = render(<TodoItem task={task} />);
 
-    it("render task content", () => {
-      const { container } = render(<TodoItem task={task} />);
+  it("render task content", () => {
+    expect(container).toHaveTextContent(task.content);
+  });
 
-      expect(container).toHaveTextContent(task.content);
+  context("click toggle btn", () => {
+    const $li = container.querySelector("li");
+    const $btn = container.querySelector(".toggle");
+    fireEvent.click($btn);
+
+    it("add class", () => {
+      expect($li).toHaveClass("completed");
     });
   });
 });
