5044bdbeedd0e13f2225e9fb039fffe2c9b5e608
hwookim <gusdn9754@gmail.com>
2020-12-31 21:02:47 +0900

    test: TodoFilter 렌더링 테스트

---
+++ src/component/TodoFilter.test.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import TodoFilter from "./TodoFilter";
+import { FILTER } from "../utils/filter";
+
+describe("TodoFilter", () => {
+  function renderFilter({ selected, onSelect }) {
+    return render(<TodoFilter selected={selected} onSelect={onSelect} />);
+  }
+
+  it("render all filter btn", () => {
+    const { container } = renderFilter({ selected: FILTER.ALL });
+
+    Object.values(FILTER).forEach((filter) =>
+      expect(container).toHaveTextContent(filter.text),
+    );
+  });
+});
