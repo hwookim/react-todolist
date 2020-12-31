2769879fff8dfefaa50fd34ca7d167d72bd34c05
hwookim <gusdn9754@gmail.com>
2020-12-31 20:19:35 +0900

    test: TodoListItem toggle 버튼 클릭 테스트

--- src/component/TodoListItem.test.jsx
+++ src/component/TodoListItem.test.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import TodoListItem from "./TodoListItem";
 
 describe("TodoListItem", () => {
-  function renderItem(todo, onToggle, onDelete) {
+  function renderItem({ todo, onToggle, onDelete }) {
     return render(
       <TodoListItem todo={todo} onToggle={onToggle} onDelete={onDelete} />,
     );
@@ -12,8 +12,19 @@ describe("TodoListItem", () => {
 
   it("render todo content", () => {
     const todo = { id: 0, content: "Let's test", completed: false };
-    const { container } = renderItem(todo);
+    const { container } = renderItem({ todo });
 
     expect(container).toHaveTextContent(todo.content);
+  });
+
+  it("run onToggle method with click toggle btn", () => {
+    const todo = { id: 0, content: "Let's test", completed: false };
+    const onToggle = jest.fn();
+    const { container } = renderItem({ todo, onToggle });
+    const $toggleBtn = container.querySelector(".toggle");
+
+    fireEvent.click($toggleBtn);
+
+    expect(onToggle).toBeCalledWith(todo.id);
   });
 });
