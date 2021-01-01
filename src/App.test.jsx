93c864bf471382d6fb7430cdec820199ff76ff1b
hwookim <gusdn9754@gmail.com>
2021-01-01 22:42:49 +0900

    test: App todo 렌더링 테스트
     - renderApp()을 통해 beforeEach 대체, 좀 더 유연한 initialStore를 가질 수 있도록 변경

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { render } from "@testing-library/react";
+import context from "jest-plugin-context";
 
 import App from "./App";
 import { FILTER } from "./utils/filter";
@@ -9,17 +10,38 @@ import { FILTER } from "./utils/filter";
 jest.mock("react-redux");
 
 describe("App", () => {
-  beforeEach(() => {
+  function renderApp({
+    todos = [],
+    filter = FILTER.ALL,
+    dispatch = jest.fn(),
+  } = {}) {
     useSelector.mockImplementation(() => ({
-      todos: [],
-      filter: FILTER.ALL,
+      todos,
+      filter,
     }));
-    useDispatch.mockImplementation(() => jest.fn());
-  });
+    useDispatch.mockImplementation(() => dispatch);
+
+    return render(<App />);
+  }
 
   it("renders title - TODOS", () => {
-    const { container } = render(<App />);
+    const { container } = renderApp();
 
     expect(container).toHaveTextContent("TODOS");
+  });
+
+  context("with todos", () => {
+    const todos = [
+      { id: 1, content: "1st Todo", completed: false },
+      { id: 2, content: "2nd Todo", completed: false },
+    ];
+
+    it("render all todos", () => {
+      const { container } = renderApp({ todos });
+
+      todos.forEach((todo) => {
+        expect(container).toHaveTextContent(todo.content);
+      });
+    });
   });
 });
