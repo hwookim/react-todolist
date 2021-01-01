fdcde798413b8b73916a074d57d9ceaef60ca872
hwookim <gusdn9754@gmail.com>
2021-01-01 22:31:57 +0900

    test: redux mocking을 통한 테스트 진행

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { render } from "@testing-library/react";
 
 import App from "./App";
-import { renderWithRedux } from "./_testUtils/render";
+import { FILTER } from "./utils/filter";
+
+jest.mock("react-redux");
 
 describe("App", () => {
+  beforeEach(() => {
+    useSelector.mockImplementation(() => ({
+      todos: [],
+      filter: FILTER.ALL,
+    }));
+    useDispatch.mockImplementation(() => jest.fn());
+  });
+
   it("renders title - TODOS", () => {
-    const { container } = renderWithRedux(<App />);
+    const { container } = render(<App />);
 
     expect(container).toHaveTextContent("TODOS");
   });
