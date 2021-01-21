5318c035310116d79b24505e732842bcd60e0ad0
hwookim <gusdn9754@gmail.com>
2021-01-21 17:55:02 +0900

    fix: 두번째 input부터 입력되지 않는 문제 해결
     - useCallback에 의해서 함수가 재사용 중이던게 문제
     - todos를 인자로 넣어 구분할 수 있지만, 결국 매번 새로운 함수를 만드는 것은 같기 때문에 무의미하다고 판단, useCallback 제거

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { renderWithRecoil } from "./_testUtils/render";
 
 import App from "./App";
+import { fireEvent } from "@testing-library/react";
 
 describe("App", () => {
   it("renders title - TODOS", () => {
@@ -10,4 +11,29 @@ describe("App", () => {
 
     expect(container).toHaveTextContent("TODOS");
   });
+
+  it("input twice, display all todos", () => {
+    const { container, getByPlaceholderText } = renderWithRecoil(<App />);
+    const $input = getByPlaceholderText("할일을 추가해주세요");
+
+    const value1 = "1st todo";
+    const value2 = "2nd todo";
+
+    addTodo($input, value1);
+    addTodo($input, value2);
+
+    expect(container).toHaveTextContent(value1);
+    expect(container).toHaveTextContent(value2);
+  });
 });
+
+function addTodo($input: HTMLElement, value: string): void {
+  fireEvent.change($input, {
+    target: { value },
+  });
+
+  fireEvent.keyDown($input, {
+    key: "Enter",
+    code: "Enter",
+  });
+}
